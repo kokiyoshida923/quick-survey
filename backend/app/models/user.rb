b84b5e87464ebd6cf9a60d698a7906996fd63191
Koki Yoshida <yoppietta@gmail.com>
2021-07-13 21:06:44 +0900

    [add] name属性, email属性を持つUserモデルを生成
    $ rails generate model User name:string email:string
    $ rails generate migration add_index_to_users_email
    $ rails db:migrate
    
    refs #14

---
+++ backend/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name,  presence: true,
+                    length: { maximum: 20, minimum: 6, allow_blank: true }
+
+  before_save { self.email = email.downcase }
+  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 255, allow_blank: true },
+                    format: { with: valid_email_regex, allow_blank: true },
+                    uniqueness: true
+end
