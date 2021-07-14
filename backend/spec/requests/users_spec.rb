edf918564c8a3db0efd2357da825b29a4d4ce18e
Koki Yoshida <yoppietta@gmail.com>
2021-07-14 19:05:34 +0900

    [test] ユーザー表示機能の挙動を保証するテストを作成
    refs #14

--- backend/spec/requests/users_spec.rb
+++ backend/spec/requests/users_spec.rb
@@ -1,4 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
+  describe "'/users/:id'にGETメソッドでリクエストを送信した場合" do
+    let!(:user) { FactoryBot.create(:user) }
+    it "ステータスコード200 (ok) が返されること" do
+      get "/api/v1/users/#{user.id}"
+      expect(response.status).to eq 200
+    end
+    it "パラメータに対応するユーザーがJSON形式で返されること" do
+      get "/api/v1/users/#{user.id}"
+      json = JSON.parse(response.body)
+      expect(json["user"]["id"]).to eq(user.id)
+    end
+  end
 end
