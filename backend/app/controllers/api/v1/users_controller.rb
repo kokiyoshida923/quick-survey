7771a2b648e3123c3af553060d9a156fe9e34798
Koki Yoshida <yoppietta@gmail.com>
2021-07-17 04:55:00 +0900

    [add] 新規ユーザー登録機能を提供するAPIを作成
    refs #16

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,27 @@ module Api
         user = User.find(params[:id])
         render json: { user: user }
       end
+
+      def new
+        user = User.new
+        render json: { user: user }
+      end
+
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: { user: user }
+        else
+          errors = user.errors.full_messages
+          render json: { errors: errors }
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
     end
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
       get "/users/:id", to: "users#show"
+      get "/signup", to: "users#new"
+      post "/signup", to: "users#create"
     end
   end
 end
