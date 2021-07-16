d1b98a0acafc2b2756563db2004ab59d2332ab78
Koki Yoshida <yoppietta@gmail.com>
2021-07-17 04:56:36 +0900

    [test] 新規ユーザー登録機能の挙動を保証するテストを作成
    refs #16

--- backend/spec/requests/users_spec.rb
+++ backend/spec/requests/users_spec.rb
@@ -13,4 +13,99 @@ RSpec.describe "Users", type: :request do
       expect(json["user"]["id"]).to eq(user.id)
     end
   end
+
+  describe "'/signup'にGETメソッドでリクエストを送信した場合" do
+    it "ステータスコード200 (ok) が返されること" do
+      get "/api/v1/signup"
+      expect(response.status).to eq 200
+    end
+    it "新規ユーザーがJSON形式で返されること" do
+      get "/api/v1/signup"
+      json = JSON.parse(response.body)
+      expect(json["user"]["id"]).to eq(nil)
+    end
+  end
+
+  describe "'/signup'にPOSTメソッドでリクエストを送信した場合" do
+    context "パラメータが妥当な場合" do
+      before do
+        @valid_name = Faker::Internet.username(specifier: 6..20, separators: %w[. _ -])
+        @valid_email = Faker::Internet.email(domain: "valid")
+        @valid_password = Faker::Internet.password(
+          min_length: 8,
+          max_length: 32,
+          mix_case: true,
+          special_characters: false
+        )
+      end
+      it "ステータスコード200 (ok) が返されること" do
+        post "/api/v1/signup", params: { user: {
+          name: @valid_name,
+          email: @valid_email,
+          password: @valid_password,
+          password_confirmation: @valid_password
+        } }
+        expect(response.status).to eq 200
+      end
+      it "作成されたユーザーがJSON形式で返されること" do
+        post "/api/v1/signup", params: { user: {
+          name: @valid_name,
+          email: @valid_email,
+          password: @valid_password,
+          password_confirmation: @valid_password
+        } }
+        json = JSON.parse(response.body)
+        expect(json["user"]["id"]).to eq(User.last.id)
+      end
+      it "Userモデルが1増加されること" do
+        expect {
+          post "/api/v1/signup", params: { user: {
+            name: @valid_name,
+            email: @valid_email,
+            password: @valid_password,
+            password_confirmation: @valid_password
+          } }
+        }.to change(User, :count).by(1)
+      end
+    end
+    context "パラメータが不当な場合" do
+      before do
+        @invalid_name = Faker::Internet.username(specifier: [(1..5), 21].sample)
+        @invalid_email = Faker::Internet.email(domain: "invalid").gsub("@", "")
+        @invalid_password = [
+          Faker::Alphanumeric.alpha(number: 7),
+          Faker::Alphanumeric.alpha(number: 33)
+        ].sample
+      end
+      it "ステータスコード200 (ok) が返されること" do
+        post "/api/v1/signup", params: { user: {
+          name: @invalid_name,
+          email: @invalid_email,
+          password: @invalid_password,
+          password_confirmation: @invalid_password
+        } }
+        expect(response.status).to eq 200
+      end
+      it "エラーメッセージがJSON形式で返されること" do
+        post "/api/v1/signup", params: { user: {
+          name: @invalid_name,
+          email: @invalid_email,
+          password: @invalid_password,
+          password_confirmation: @invalid_password
+        } }
+        json = JSON.parse(response.body)
+        expect(json["errors"]).to be_truthy
+      end
+      it "Userモデルが増減しないこと" do
+        expect {
+          post "/api/v1/signup", params: { user: {
+            name: @invalid_name,
+            email: @invalid_email,
+            password: @invalid_password,
+            password_confirmation: @invalid_password
+          } }
+        }.to_not change(User, :count)
+      end
+    end
+  end
 end
