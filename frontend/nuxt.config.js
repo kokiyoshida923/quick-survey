04810e980d0dc509174cffdacbeddea5f2ef01b6
Koki Yoshida <yoppietta@gmail.com>
2021-07-20 12:52:14 +0900

    [update] 開発環境におけるオリジン間リソース共有 (CORS)に認証情報を許可する設定を追加
    refs #20

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -51,7 +51,9 @@ export default {
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    credentials: true,
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
