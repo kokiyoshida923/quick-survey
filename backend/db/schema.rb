8d01bd00357048d6c592fdf5894eabe779b35e63
Koki Yoshida <yoppietta@gmail.com>
2021-07-23 14:47:11 +0900

    [add] Userモデルにremember_me属性を追加
    $ rails generate migration add_remember_digest_to_users remember_digest:string
    $ rails db:migrate
    
    refs #26

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_121017) do
+ActiveRecord::Schema.define(version: 2021_07_23_054107) do
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_07_13_121017) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar"
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
