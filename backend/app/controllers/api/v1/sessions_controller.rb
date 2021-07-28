bf2d209206d1e127030626035e6aa55d52be8fab
Koki Yoshida <yoppietta@gmail.com>
2021-07-29 08:47:00 +0900

    [add] 永続的なユーザー認証機能と永続的なユーザー認証破棄機能を提供するAPIを作成
    refs #26

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -10,7 +10,7 @@ module Api
       end
 
       def new
-        unauth_user = Session.new(email: nil, password: nil)
+        unauth_user = Session.new(email: nil, password: nil, remember_me: nil)
         render json: { unauth_user: unauth_user }
       end
 
@@ -19,6 +19,7 @@ module Api
         if unauth_user.valid?
           auth_user = User.find_by(email: params[:session][:email].downcase)
           session[:user_id] = auth_user.id
+          params[:session][:remember_me] == "1" ? remember(auth_user) : forget(auth_user)
           render json: { auth_user: auth_user }
         else
           errors = unauth_user.errors.full_messages
@@ -29,6 +30,7 @@ module Api
       def destroy
         return unless @current_user
 
+        forget(@current_user)
         session.delete(:user_id)
         @current_user = nil
       end
@@ -36,7 +38,19 @@ module Api
       private
 
       def session_params
-        params.require(:session).permit(:email, :password)
+        params.require(:session).permit(:email, :password, :remember_me)
+      end
+
+      def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+      end
+
+      def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
       end
     end
   end
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::API
   before_action :current_user
 
   def current_user
-    return unless (user_id = session[:user_id])
-
-    @current_user ||= User.find_by(id: user_id)
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        session[:user_id] = user.id
+        @current_user = user
+      end
+    end
   end
 end
