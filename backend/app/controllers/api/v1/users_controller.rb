d8e828ea5c491d66f7d26297bc79a3e6608f5e11
Koki Yoshida <yoppietta@gmail.com>
2021-08-12 15:57:35 +0900

    [add] 新規ユーザー登録時に自動的に認証状態を保持する機能を追加
    refs #50

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -17,6 +17,7 @@ module Api
       def create
         user = User.new(user_params)
         if user.save
+          session[:user_id] = user.id
           render json: { user: user }
         else
           errors = user.errors.full_messages
