626aa21ce5b58fb030c83f1526dfd47fb942a1dd
Koki Yoshida <yoppietta@gmail.com>
2021-08-27 10:19:54 +0900

    [add] 複数のユーザーを初期データとして投入するための処理をseedファイルに追加
    refs #76

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "Example User",
+  email: "user@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+(2..70).each do |n|
+  name  = Faker::Internet.username(specifier: 6..20, separators: %w[. _ -])
+  email = "user-#{n}@example.com"
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
+
+seed_dir = Rails.root.join("public/seed_#{Rails.env}/").to_s
+uploads_dir = Rails.root.join("public/uploads_#{Rails.env}/").to_s
+FileUtils.rm_r(seed_dir) if FileTest.exist?(seed_dir)
+FileUtils.rm_r(uploads_dir) if FileTest.exist?(uploads_dir)
+FileUtils.mkdir_p(seed_dir)
+
+User.all.each do |user|
+  seed_avatar_url = Faker::Avatar.image(slug: user.id, format: "jpg").split(/\?/).first
+  seed_avatar_file = File.basename(seed_avatar_url)
+  seed_avatar_path = seed_dir + seed_avatar_file
+  File.open(seed_avatar_path, "wb") do |output|
+    URI.parse(seed_avatar_url).open do |data|
+      output.write(data.read)
+      puts seed_avatar_url
+    end
+  end
+
+  user.update!(
+    avatar: File.open(Rails.root.join(seed_avatar_path))
+  )
+end
